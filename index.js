a9f42344dbd739ca2ed7737e346624ce032e5d83
einnad <146109295+einnad@users.noreply.github.com.>
2024-01-05 11:50:50 +0000

    Add starting files

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
