3aafb0ffd64f4d94b66bf7b4e6f9cac0df278b15
einnad <146109295+einnad@users.noreply.github.com.>
2024-01-06 00:38:08 +0000

    Fix links and add post feature
    Create and explore route correctly. Create blog posts to index; ignore
    placeholder text

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
-app.get('/create', (req, res) => {
-  res.render("create.ejs");
+app.get("/create", (req, res) => {
+  res.render("create");
 });
 
-app.get('/explore', (req, res) => {
-  res.render("explore.ejs");
+app.get("/explore", (req, res) => {
+  res.render("explore");
+});
+
+app.post('/submit', (req, res) => {
+  console.log(req.body["content"]);
+  const blogPost = req.body["content"];
+  res.render("index", {post: blogPost});
 });
